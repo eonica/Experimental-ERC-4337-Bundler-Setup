80fdfaac47ea1c9e6d17069cc24e7a964e6ec965
Emanuel Onica <37279870+eonica@users.noreply.github.com>
2025-12-01 07:06:39 +0200

    EntryPoint and Factory loader

---
+++ EPLoader.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "https://github.com/eth-infinitism/account-abstraction/blob/v0.6.0/contracts/core/EntryPoint.sol";
+import "https://github.com/eth-infinitism/account-abstraction/blob/v0.6.0/contracts/samples/SimpleAccountFactory.sol";
